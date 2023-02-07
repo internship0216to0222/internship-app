069256af051d9a2c70204ed16353581bf0ff9771
intern3 <internship0216to0222@gmail.com>
2023-02-07 16:11:32 +0900

    initial commit

---
+++ src/main/java/com/example/internshipapp/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.internshipapp.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping("")
+    public String main() {
+        return "index.html";
+    }
+}
