dd11a00c43abf370a1a18e056ee48342324cb14d
intern3 <internship0216to0222@gmail.com>
2023-02-17 09:32:16 +0900

    User entity の作成

---
+++ src/main/java/com/example/internshipapp/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.internshipapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+    private int id;
+
+    private String email_address;
+
+    private String phone_number;
+
+    private String bank;
+
+    private String branch;
+
+    private int account_type;
+
+    private String account_number;
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
+
+}
