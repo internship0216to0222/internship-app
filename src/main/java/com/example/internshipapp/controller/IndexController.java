57f41f3e6f693a483a1930b777e1c36bbea9d731
qaw-staff-admin <internship0216to0222@gmail.com>
2023-02-17 10:08:32 +0900

    RepositoryとControllerの作成、データベースの呼び出し

--- src/main/java/com/example/internshipapp/controller/IndexController.java
+++ src/main/java/com/example/internshipapp/controller/IndexController.java
@@ -1,17 +1,25 @@
 package com.example.internshipapp.controller;
 
+import com.example.internshipapp.entity.User;
+import com.example.internshipapp.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @AllArgsConstructor
 @RequestMapping("/")
 public class IndexController {
+    UserRepository userRepository;
 
     @GetMapping("")
+
     public String main() {
+        List<User> users = userRepository.findAll();
+        System.out.println(users);
         return "index.html";
     }
 }
